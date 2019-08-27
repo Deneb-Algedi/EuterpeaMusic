3a1eab183e4990041e0de56bc0083b6b50e815a2
Deneb-Algedi <46696752+Deneb-Algedi@users.noreply.github.com>
2019-08-27 10:20:23 -0400

    Add files via upload

---
+++ verde.hs
@@ -0,0 +1,11 @@
+import Euterpea
+
+
+melody :: Music Pitch
+melody = line [e 4 en, fs 4 en, g 4 en, a 4 qn, b 4 en, c 5 qn, b 4 en, a 4 qn, g 4 qn, a 4 en, b 4 qn, a 4 en, g 4 qn, fs 4 qn, g 4 en, a 4 qn, g 4 en, a 4 qn, b 4 hn, d 4 en, e 4 en, fs 4 en, g 4 en, a 4 qn, b 4 en, c 5 qn, b 4 en, a 4 qn, g 4 qn, a 4 en, b 4 qn, a 4 en, g 4 qn, fs 4 qn, b 4 en, c 5 en, b 4 en, a 4 en, g 4 en, fs 4 en, e 4 qn, rest qn, g 4 qn, b 4 en, e 5 qn, b 4 en, g 4 qn, e 4 qn, rest qn, g 4 dqn, a 4 en, b 4 dhn, e 5 qn, b 4 hn, a 4 hn, g 4 wn, rest hn, g 4 dqn, a 4 en, b 4 dhn, e 5 qn, b 4 hn, a 4 hn, g 4 dwn, rest qn, a 4 en, b 4 en, c 5 dqn, c 5 qn, c 5 en, c 5 qn, c 5 dqn, d 5 dqn, d 5 en, c 5 en, c 5 dqn, b 4 dhn, rest dhn, fs 4 en, g 4 en, a 4 dqn, a 4 qn, a 4 en, a 4 qn, c 5 dqn, b 4 qn, a 4 en, b 4 qn, a 4 dqn, g 4 dhn, rest qn, a 4 qn, c 5 qn, e 4 qn, g 4 wn, rest qn, fs 4 qn, b 4 qn, ds 4 qn, e 4 hn]
+
+
+
+
+verde :: Music Pitch
+verde = melody
